e55e4d839c17720beb0665bd9188d44d6a51e6b9
Giovanna Saúd <gicsaud1410@gmail.com>
2023-12-20 22:51:04 -0300

    final project

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
+const dialogflow = require('./dialogflow');
+const youtube = require('./youtube')
+
 const token = '6854209702:AAGJkIKoHamDWdVFOpQb1O6Yn5O3CFqygAM';
+
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', function (msg) {
+bot.on('message', async function (msg) {
     const chatId = msg.chat.id;
     console.log(msg.text);
-    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
-})
+
+    const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
+
+
+    let responseText = dfResponse.text;
+
+    if(dfResponse.intent === 'Treino especifico'){
+        responseText = await youtube.searchVideoURL(responseText, dfResponse.fields.corpo.stringValue)
+    }
+
+    bot.sendMessage(chatId, responseText);
+});
